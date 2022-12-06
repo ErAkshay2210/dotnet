b7776e143891149f442cfe2a72f923cc0f8dbda3
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2022-12-06 14:14:36 +0530

    mysql connection with .net

---
+++ MySQLConnection/Program.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+
+string conStr ="server =localhost; uid = root; password= root; database=classicmodels";
+
+MySqlConnection con = new MySqlConnection();
+
+
+try{
+
+    con.ConnectionString=conStr;
+    con.Open();
+    string query ="SELECT * FROM  CUSTOMERS";
+    MySqlCommand cmd = new MySqlCommand(query, con);
+    MySqlDataReader rdr= cmd.ExecuteReader();
+    while (rdr.Read())
+    {
+        System.Console.WriteLine(rdr[0]+"---"+rdr[1]+"---");
+    }
+    rdr.Close();
+}
+catch(Exception ee)
+{
+    System.Console.WriteLine(ee.Message);
+}
+finally
+{
+    con.Clone();
+}
