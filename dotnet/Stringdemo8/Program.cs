bcaa96f912edc58cd2ba4fff9b0ec35e68e27239
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2022-12-11 22:06:42 +0530

    stringDemoApp

---
+++ dotnet/Stringdemo8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Collections.Generic; 
+
+string str1;
+
+
+string str2 = "Welcome to trasflower"
+
+string str3 ="hello World"
+
+string str4= string.Empty;
+
+
+String str5 = null;
+
+char[] letters = { 'A', 'K', 'S','H','A','Y'};
+string str6 = new string(letters);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
